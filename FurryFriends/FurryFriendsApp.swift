7b92254a8423076778594d636b379b2125ad0760
Alice <xinranjiang2015@yahoo.com>
2023-06-18 12:21:29 -0400

    Initial Commit

---
+++ FurryFriends/FurryFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurryFriendsApp.swift
+//  FurryFriends
+//
+//  Created by apple on 2023/6/18.
+//
+
+import SwiftUI
+
+@main
+struct FurryFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
